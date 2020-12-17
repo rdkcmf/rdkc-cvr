c0fd25a2ca554b64dd4b1539c4fc832ad0746889
sthiru200 <subburaj_thirugowrisankar@comcast.com>
2020-12-17 19:07:20 +0530

    RDKC-8600: Support for Dynamic RDK logging for DBC
    Reason for change: Add handler for
    enabling dynamic logging.
    
    Test Procedure: Ensure that the dynamic logging
    for CVR works.
    Risks: None
    
    Signed-off-by: subburaj <subburaj_thirugowrisankar@comcast.com>
    (cherry picked from commit dd3bb61ee571df1d02ea3690908cebe84d07e45d)
    Change-Id: I99600c7b83c2d66a9362dfac52bbf2b239bea6c0

--- kvs/frameupload/core/osi_cvr_daemon.cpp
+++ kvs/frameupload/core/osi_cvr_daemon.cpp
@@ -1053,6 +1053,7 @@ int CVR::cvr_init(unsigned short isAudio,cvr_provision_info_t *pCloudRecorderInf
         rtConnection_Create(&connectionRecv, "CVR_RECV", "tcp://127.0.0.1:10001");
         rtConnection_AddListener(connectionRecv, "RDKC.CVR", on_message_cvr, NULL);
         rtConnection_AddListener(connectionRecv, "RDKC.SMARTTN.STATUS", on_message_smt_TN, NULL);
+        rtConnection_AddListener(connectionRecv, "RDKC.ENABLE_DYNAMIC_LOG", on_message_dyn_log, connectionRecv);
 
         std::thread rtMessage_recv_thread (receive_rtmessage);
         pthread_setname_np(rtMessage_recv_thread.native_handle(),"rt_recmessage");
